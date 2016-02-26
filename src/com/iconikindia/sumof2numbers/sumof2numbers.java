f4521b2ffec8d258566767814ae75a8dd1413406
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 22:45:09 +0530

    Sum of 2 numbers
    Sum of 2 numbers

---
+++ src/com/iconikindia/sumof2numbers/sumof2numbers.java
@@ -0,0 +1,11 @@
+package com.iconikindia.sumof2numbers;
+
+public class sumof2numbers {
+
+	public static void main(String[] args) {
+		int a = 10;
+		float b = 10.55f;
+		float sum = a+b;
+		System.out.println("Sum Of 2 numbers are : "+sum);
+	}
+}
