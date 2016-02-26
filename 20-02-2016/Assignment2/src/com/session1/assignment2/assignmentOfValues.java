166cc936862f07187ef05af6ea0ba24e559b7ab2
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 22:59:23 +0530

    assignment2
    assignment2

---
+++ 20-02-2016/Assignment2/src/com/session1/assignment2/assignmentOfValues.java
@@ -0,0 +1,9 @@
+package com.session1.assignment2;
+
+public class assignmentOfValues {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=b=c=4;
+		System.out.println("Value is b : "+b);
+	}
+}
