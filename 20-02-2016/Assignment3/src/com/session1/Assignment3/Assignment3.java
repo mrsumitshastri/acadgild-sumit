80804d970e96943053311d399934dc97c78e6efe
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 23:07:30 +0530

    Assignment3
    Assignment3

---
+++ 20-02-2016/Assignment3/src/com/session1/Assignment3/Assignment3.java
@@ -0,0 +1,9 @@
+package com.session1.Assignment3;
+
+public class Assignment3 {
+	public static void main(String[] args) {
+		String str = "Sumit";
+		int a = (int)str;
+		// i woulld like to store str in a but it is not possible see error
+	}
+}
