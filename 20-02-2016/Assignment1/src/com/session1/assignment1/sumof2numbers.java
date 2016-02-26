ad0785ec687c0e644bdb2057541b4d5175fbc75a
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 22:51:36 +0530

    Commit
    Commit

--- 20-02-2016/Assignment1/src/com/session1/assignment1/sumof2numbers.java
+++ 20-02-2016/Assignment1/src/com/session1/assignment1/sumof2numbers.java
@@ -1,11 +1,11 @@
-package com.iconikindia.sumof2numbers;
+package com.session1.assignment1;
 
 public class sumof2numbers {
-
 	public static void main(String[] args) {
 		int a = 10;
 		float b = 10.55f;
 		float sum = a+b;
 		System.out.println("Sum Of 2 numbers are : "+sum);
 	}
+
 }
